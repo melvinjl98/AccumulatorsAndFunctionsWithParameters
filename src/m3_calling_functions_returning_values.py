1c107023c72927b6698bd2553e70b9b80d70c723
melvinjl <melvinjl@rose-hulman.edu>
2017-12-04 02:10:43 -0500

    m6 done

--- src/m3_calling_functions_returning_values.py
+++ src/m3_calling_functions_returning_values.py
@@ -7,8 +7,8 @@ This module demonstrates and practices:
   -- UNIT TESTING.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Josh Melvin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m3t_tester
 
@@ -37,7 +37,7 @@ def main():
 def run_test_sum_of_digits():
     """ Tests the  sum_of_digits   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function, as follows:
+    # DONE: 2. Implement this TEST function, as follows:
     #
     #  Step 1:  This TEST function tests the  sum_of_digits  function.
     #    So read the doc-string of the  sum_of_digits  function
@@ -67,10 +67,34 @@ def run_test_sum_of_digits():
     print('Test 1 expected:', expected)
     print('       actual:  ', answer)
 
+    #mytest1
+    expected = 14
+    answer = sum_of_digits(923)
+    print('mytest1 expected:', expected)
+    print('       actual:  ', answer)
+
+
     # ------------------------------------------------------------------
-    # TO DO: 2 (continued).
+    # DONE: 2 (continued).
     # Below this comment, add 3 more test cases of your own choosing.
     # ------------------------------------------------------------------
+    #mytest2
+    expected = 13
+    answer = sum_of_digits(472)
+    print('mytest2 expected:', expected)
+    print('       actual:  ', answer)
+
+    #mytest3
+    expected = 19
+    answer = sum_of_digits(5284)
+    print('mytest3 expected:', expected)
+    print('       actual:  ', answer)
+
+    #mytest4
+    expected = 20
+    answer = sum_of_digits(1478)
+    print('mytest4 expected:', expected)
+    print('       actual:  ', answer)
 
 
 def sum_of_digits(number):
@@ -109,7 +133,7 @@ def sum_of_digits(number):
 def run_test_digits_in_cube():
     """ Tests the   digits_in_cube   function. """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this function.
+    # DONE: 3. Implement this function.
     #   It TESTS the  digits_in_cube  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -134,6 +158,24 @@ def run_test_digits_in_cube():
     print('Testing the   digits_in_cube   function:')
     print('-----------------------------------------------------')
 
+    #mytest1
+    expected = 10
+    answer = digits_in_cube(4)
+    print('mytest1 expected:', expected)
+    print('       actual:  ', answer)
+
+    #mytest2
+    expected = 9
+    answer = digits_in_cube(6)
+    print('mytest2 expected:', expected)
+    print('       actual:  ', answer)
+
+    #mytest3
+    expected = 10
+    answer = digits_in_cube(7)
+    print('mytest3 expected:', expected)
+    print('       actual:  ', answer)
+
 
 def digits_in_cube(n):
     """
@@ -145,7 +187,7 @@ def digits_in_cube(n):
       this function returns (1 + 2 + 5), which is 8.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
@@ -154,6 +196,17 @@ def digits_in_cube(n):
     #    the    sum_of_digits    function that is DEFINED ABOVE.
     ####################################################################
     # ------------------------------------------------------------------
+    if n < 0:
+        n = -n
+
+    while True:
+        if n == 0:
+            break
+        digit_cube = n**3
+        n = n // 10
+        digit_sum_cube = sum_of_digits(digit_cube)
+
+    return digit_sum_cube
 
 
 def run_test_digits_in_power():
@@ -169,6 +222,24 @@ def run_test_digits_in_power():
     print('--------------------------------------------------')
     print('Testing the   digits_in_power   function:')
     print('--------------------------------------------------')
+
+    #mytest1
+    expected = 36
+    answer = digits_in_power(6, 7)
+    print('mytest1 expected:', expected)
+    print('       actual:  ', answer)
+
+    #mytest2
+    expected = 19
+    answer = digits_in_power(8, 4)
+    print('mytest1 expected:', expected)
+    print('       actual:  ', answer)
+
+    #mytest3
+    expected = 9
+    answer = digits_in_power(3, 5)
+    print('mytest1 expected:', expected)
+    print('       actual:  ', answer)
 
 
 def digits_in_power(n, k):
@@ -190,6 +261,16 @@ def digits_in_power(n, k):
     #    the    sum_of_digits    function that is DEFINED ABOVE.
     ####################################################################
     # ------------------------------------------------------------------
+    if n < 0:
+        n = -n
+
+    while True:
+        if n== 0:
+            break
+        x = n**k
+        digit_power = sum_of_digits(x)
+
+    return digit_power
 
 
 def run_test_fancy_sums_of_digits():
--- src/m6_summing.py
+++ src/m6_summing.py
@@ -4,8 +4,10 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Josh Melvin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+import math
 
 
 def main():
@@ -17,7 +19,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +30,26 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
+
+    #test1
+    expected = -0.51948
+    answer = sum_cosines(4)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+
+    #test2
+    expected = -0.41745
+    answer = sum_cosines(10)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+
+
+    #test3
+    expected = 1.33275
+    answer = sum_cosines(8)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
 
 
 def sum_cosines(n):
@@ -41,19 +63,24 @@ def sum_cosines(n):
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_cosines  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    total = math.cos(0)
+    for k in range(n):
+        total = total + math.cos(k + 1)
+
+    return total
 
 
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -64,6 +91,26 @@ def run_test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
+
+    #test1
+    expected =8.69213
+    answer = sum_square_roots(4)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+
+    #test2
+    expected = 31.77494
+    answer = sum_square_roots(10)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+
+
+    #test3
+    expected = 23.06017
+    answer = sum_square_roots(8)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
 
 
 def sum_square_roots(n):
@@ -80,14 +127,18 @@ def sum_square_roots(n):
       which is about 11.854408.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_square_roots  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(n):
+        total = total + math.sqrt(2*(k + 1))
 
+    return total
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
